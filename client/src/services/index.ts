5ba40cfe05bcb916b2deee3520a9b1c6125aabf0
kanishktrojan <kanishktrojan@gmail.com>
2026-01-02 19:56:37 +0530

    Bug fixed

--- client/src/services/index.ts
+++ client/src/services/index.ts
@@ -1,6 +1,5 @@
 export { default as api } from './api';
 export { authService } from './authService';
 export { tmdbService } from './tmdbService';
-export { tmdbDirectService } from './tmdbDirectService';
 export { streamService } from './streamService';
 export { userContentService } from './userContentService';
--- client/src/vite-env.d.ts
+++ client/src/vite-env.d.ts
@@ -2,7 +2,6 @@
 
 interface ImportMetaEnv {
   readonly VITE_API_URL: string;
-  readonly VITE_TMDB_API_KEY: string;
   readonly DEV: boolean;
   readonly PROD: boolean;
   readonly MODE: string;
